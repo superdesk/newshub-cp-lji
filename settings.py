8c4821cb258b156406fbda0376763ed94173aaf2
Petr <petr.jasek@sourcefabric.org>
2019-11-27 16:28:16 +0100

    add fr_CA translation

--- settings.py
+++ settings.py
@@ -1,4 +1,3 @@
-
 import os
 from newsroom.default_settings import CLIENT_CONFIG
 from flask_babel import gettext
@@ -7,43 +6,40 @@ env = os.environ.get
 
 DEBUG = False
 
-MAIL_SERVER = env('MAIL_SERVER', 'localhost')
-MAIL_PORT = int(env('MAIL_PORT', 25))
-MAIL_USE_TLS = env('MAIL_USE_TLS', False)
-MAIL_USE_SSL = env('MAIL_USE_SSL', False)
-MAIL_USERNAME = env('MAIL_USERNAME', '')
-MAIL_PASSWORD = env('MAIL_PASSWORD', '')
+MAIL_SERVER = env("MAIL_SERVER", "localhost")
+MAIL_PORT = int(env("MAIL_PORT", 25))
+MAIL_USE_TLS = env("MAIL_USE_TLS", False)
+MAIL_USE_SSL = env("MAIL_USE_SSL", False)
+MAIL_USERNAME = env("MAIL_USERNAME", "")
+MAIL_PASSWORD = env("MAIL_PASSWORD", "")
 
-CLIENT_TIME_FORMAT = 'HH:mm'
-CLIENT_DATE_FORMAT = 'MMM DD, YYYY'
-SITE_NAME = 'CP Newsroom LJI'
+CLIENT_TIME_FORMAT = "HH:mm"
+CLIENT_DATE_FORMAT = "MMM DD, YYYY"
+SITE_NAME = "CP Newsroom LJI"
 SHOW_USER_REGISTER = True
-COPYRIGHT_HOLDER = 'CP'
-COPYRIGHT_NOTICE = ''
-USAGE_TERMS = ''
-LANGUAGES = ['en', 'fr_CA']
-DEFAULT_LANGUAGE = 'en'
-CLIENT_CONFIG['list_animations'] = False
+COPYRIGHT_HOLDER = "CP"
+COPYRIGHT_NOTICE = ""
+USAGE_TERMS = ""
+LANGUAGES = ["en", "fr_CA"]
+DEFAULT_LANGUAGE = "en"
+CLIENT_CONFIG["list_animations"] = False
 
 
-PRIVACY_POLICY = PRIVACY_POLICY_EN = 'https://www.thecanadianpress.com/privacy-policy/'
-TERMS_AND_CONDITIONS = TERMS_AND_CONDITIONS_EN = \
-    'https://www.thecanadianpress.com/content-services/custom-content-creation/terms-conditions/'
-CONTACT_ADDRESS = 'https://www.thecanadianpress.com/contact/'
-CONTACT_ADDRESS_EN = 'https://www.thecanadianpress.com/contact/'
+PRIVACY_POLICY = PRIVACY_POLICY_EN = "https://www.thecanadianpress.com/privacy-policy/"
+TERMS_AND_CONDITIONS = (
+    TERMS_AND_CONDITIONS_EN
+) = "https://www.thecanadianpress.com/content-services/custom-content-creation/terms-conditions/"
+CONTACT_ADDRESS = "https://www.thecanadianpress.com/contact/"
+CONTACT_ADDRESS_EN = "https://www.thecanadianpress.com/contact/"
 
 WIRE_AGGS = {
-    'service': {'terms': {'field': 'service.name', 'size': 50}},
-    'subject': {'terms': {'field': 'subject.name', 'size': 20}},
+    "service": {"terms": {"field": "service.name", "size": 50}},
+    "subject": {"terms": {"field": "subject.name", "size": 20}},
 }
 
 WIRE_GROUPS = [
-    {
-        'field': 'service',
-        'label': gettext('Category'),
-    },
-    {
-        'field': 'subject',
-        'label': gettext('Subject'),
-    }
+    {"field": "service", "label": gettext("Category"),},
+    {"field": "subject", "label": gettext("Subject"),},
 ]
+
+BABEL_TRANSLATION_DIRECTORIES = os.path.join(os.path.dirname(__file__), "translations",)
